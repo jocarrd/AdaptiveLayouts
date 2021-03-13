90b07ed99c24484967cfef40f92acd7f2a38916e
Jorge Carrera Diez <55456507+jocarrd@users.noreply.github.com>
2021-03-13 20:18:19 +0100

    Fin AdaptativeLayouts

--- app/src/main/java/com/example/android/materialme/MainActivity.java
+++ app/src/main/java/com/example/android/materialme/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.materialme;
 import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
@@ -25,12 +26,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        int gridColumnCount = getResources().getInteger(R.integer.grid_column_count);
         // Initialize the RecyclerView.
         mRecyclerView = findViewById(R.id.recyclerView);
 
         // Set the Layout Manager.
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setLayoutManager(new
+                GridLayoutManager(this, gridColumnCount));
 
         // Initialize the ArrayList that will contain the data.
         mSportsData = new ArrayList<>();
@@ -41,14 +43,19 @@ public class MainActivity extends AppCompatActivity {
 
         // Get the data.
         initializeData();
+        int swipeDirs;
+        if(gridColumnCount > 1){
+            swipeDirs = 0;
+        } else {
+            swipeDirs = ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT;
+        }
 
         // Helper class for creating swipe to dismiss and drag and drop
         // functionality.
-        ItemTouchHelper helper = new ItemTouchHelper(new ItemTouchHelper
-                .SimpleCallback(
-                ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT |
-                        ItemTouchHelper.DOWN | ItemTouchHelper.UP,
-                ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+        ItemTouchHelper helper = new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(ItemTouchHelper.LEFT |
+                                                                     ItemTouchHelper.RIGHT |
+                                                                     ItemTouchHelper.DOWN | ItemTouchHelper.UP,
+                                                                     swipeDirs) {
             /**
              * Defines the drag and drop functionality.
              *
